a1374b6a91bb3e326a076a6d4a5c25696f40ec21
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-24 16:49:37 +0500

    Create header

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-import './bootstrap';
+import "./bootstrap";
+
+function displayOrNot(block) {
+    return block.style.display;
+}
+
+profileBtn.addEventListener("click", function () {
+    if (displayOrNot(profileMenu) == 'flex') {
+        profileMenu.style.display = "none";
+    } else {
+        profileMenu.style.display = "flex";
+    }
+});
+
+profileCross.addEventListener('click', function() {
+    profileMenu.style.display = "none";
+})
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,10 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/main.css">
     @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
+    <header>
+        <div class="logo">
+            <img src="{{ URL::asset('/image/logo_example.png') }}" alt="Logo">
+            <p>
+                Починим!
+                <br>
+                Сервисный центр
+            </p>
+        </div>
+        <nav>
+            <a href="#">Главная</a>
+            <a href="#">Услуги</a>
+            <a href="#">О нас</a>
+            <a href="#">Контакты</a>
+            <img id="profileBtn" src="{{ URL::asset('/image/profile.svg') }}" alt="Profile">
+            <div id="profileMenu">
+                <img id="profileCross" src="{{ URL::asset('/image/cross.svg') }}" alt="cross">
+                <a href="">Войти</a>
+                <a href="">Зарегестрироваться</a>
+            </div>
+        </nav>
+    </header>
     @yield('content')
 </body>
 </html>
