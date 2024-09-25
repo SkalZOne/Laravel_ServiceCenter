fc3c79e0aedbccc92768c93e2b8cde8e8428d2a5
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-25 13:35:34 +0500

    Verst Refactoring

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,28 +6,12 @@
     @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <header>
-        <div class="logo">
-            <img src="{{ URL::asset('/image/logo_example.png') }}" alt="Logo">
-            <p>
-                Починим!
-                <br>
-                Сервисный центр
-            </p>
-        </div>
-        <nav>
-            <a href="#">Главная</a>
-            <a href="#">Услуги</a>
-            <a href="#">О нас</a>
-            <a href="#">Контакты</a>
-            <img id="profileBtn" src="{{ URL::asset('/image/profile.svg') }}" alt="Profile">
-            <div id="profileMenu">
-                <img id="profileCross" src="{{ URL::asset('/image/cross.svg') }}" alt="cross">
-                <a href="">Войти</a>
-                <a href="">Зарегестрироваться</a>
-            </div>
-        </nav>
+    <header class="header">
+        
     </header>
-    @yield('content')
+    <main class="main">
+        @yield('content')
+    </main>
+    <footer class="footer"></footer>
 </body>
 </html>
