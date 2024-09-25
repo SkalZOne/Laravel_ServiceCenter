7e15c3f8c1a77bdd66c242e462a49c9c997555d9
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-25 14:48:35 +0500

    Header_InWork added navigation; added title; added components: 4dots & button-out;

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,17 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
     <header class="header">
-        
+        <nav class="header__navigation">
+            <div class="header__navigation__links">
+                <a href="#" class="header__navigation__link">Главная</a>
+                <a href="#" class="header__navigation__link">Оклейка автомобилей</a>
+                <a href="#" class="header__navigation__link">Детейлинг автомобилей</a>
+                <a href="#" class="header__navigation__link">Галерея работ</a>
+            </div>
+            <img class="header__navigation__burger-menu" src="{{ URL::asset('/image/burger-menu.svg') }}"
+                alt="burgerMenu">
+        </nav>
+        <h1 class="header__title">CAR MUSC</h1>
+        <div class="header__dots four-dots">
+            <img src="{{ URL::asset('/image/red-dot.svg') }}" alt="dot">
+            <img src="{{ URL::asset('/image/red-dot.svg') }}" alt="dot">
+            <img src="{{ URL::asset('/image/red-dot.svg') }}" alt="dot">
+            <img src="{{ URL::asset('/image/red-dot.svg') }}" alt="dot">
+        </div>
+        <p class="header__under-title-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae orci urna
+            amet penatibus.</p>
+        <button class="header__order-btn btn-out">
+            <p class="btn-out__text">Наши услуги</p>
+        </button>
     </header>
     <main class="main">
         @yield('content')
     </main>
     <footer class="footer"></footer>
 </body>
+
 </html>
