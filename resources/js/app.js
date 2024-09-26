1e842ecfadccd6fa89a7d3d6dfe376c5c18ccc09
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-26 16:19:57 +0500

    added functional to services cards

--- resources/js/app.js
+++ resources/js/app.js
@@ -48,3 +48,70 @@ thirdSliderRightArrow.addEventListener("click", function () {
 
     thirdSliderText.textContent = '123'
 });
+
+// Services cards
+servicesLeftBtn.addEventListener('click', function() {
+    let innerText = `<div class="fourth-block__services-cards__first-card">
+            <img class="fourth-block__services-cards__first-card__image" src="/image/car-washing.svg" alt="carWashing">
+            <p class="fourth-block__services-cards__first-card__title">Защитные пленки</p>
+            <p class="fourth-block__services-cards__first-card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nascetur ultrices pellentesque vulputate sit. Ut feugiat.</p>
+            <button class="fourth-block__services-cards__first-card__description-button btn-out"><p class="btn-out__text">Подробнее</p></button>
+        </div>
+        <div class="fourth-block__services-cards__second-card">
+        <img class="fourth-block__services-cards__first-card__image" src="/image/blank-car.svg" alt="blankCar">
+            <p class="fourth-block__services-cards__first-card__title">Цветные пленки</p>
+            <p class="fourth-block__services-cards__first-card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nascetur ultrices pellentesque vulputate sit. Ut feugiat.</p>
+            <button class="fourth-block__services-cards__first-card__description-button btn-out"><p class="btn-out__text">Подробнее</p></button>
+        </div>
+        <div class="fourth-block__services-cards__third-card">
+        <img class="fourth-block__services-cards__first-card__image" src="/image/oil-spray.svg" alt="oilSpray">
+            <p class="fourth-block__services-cards__first-card__title">Дизайн</p>
+            <p class="fourth-block__services-cards__first-card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nascetur ultrices pellentesque vulputate sit. Ut feugiat.</p>
+            <button class="fourth-block__services-cards__first-card__description-button btn-out"><p class="btn-out__text">Подробнее</p></button>
+        </div>
+        <div class="fourth-block__services-cards__fourth-card">
+        <img class="fourth-block__services-cards__first-card__image" src="/image/car-check.svg" alt="carCheck">
+            <p class="fourth-block__services-cards__first-card__title">Оклейка салона</p>
+            <p class="fourth-block__services-cards__first-card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nascetur ultrices pellentesque vulputate sit. Ut feugiat.</p>
+            <button class="fourth-block__services-cards__first-card__description-button btn-out"><p class="btn-out__text">Подробнее</p></button>
+        </div>`
+
+    if (!(servicesCards.innerHTML == innerText)) {
+        servicesLeftBtn.classList.add('btn-active')
+        servicesRightBtn.classList.remove('btn-active')
+        servicesCards.innerHTML = innerText
+    }
+})
+
+servicesRightBtn.addEventListener('click', function() {
+    let innerText = `<div class="fourth-block__services-cards__first-card">
+            <img class="fourth-block__services-cards__first-card__image" src="/image/car-washing.svg" alt="carWashing">
+            <p class="fourth-block__services-cards__first-card__title">Установка внештатного оборудования</p>
+            <p class="fourth-block__services-cards__first-card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nascetur ultrices pellentesque vulputate sit. Ut feugiat.</p>
+            <button class="fourth-block__services-cards__first-card__description-button btn-out"><p class="btn-out__text">Подробнее</p></button>
+        </div>
+        <div class="fourth-block__services-cards__second-card">
+        <img class="fourth-block__services-cards__first-card__image" src="/image/blank-car.svg" alt="blankCar">
+            <p class="fourth-block__services-cards__first-card__title">Проверка и консультация со специалистами</p>
+            <p class="fourth-block__services-cards__first-card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nascetur ultrices pellentesque vulputate sit. Ut feugiat.</p>
+            <button class="fourth-block__services-cards__first-card__description-button btn-out"><p class="btn-out__text">Подробнее</p></button>
+        </div>
+        <div class="fourth-block__services-cards__third-card">
+        <img class="fourth-block__services-cards__first-card__image" src="/image/oil-spray.svg" alt="oilSpray">
+            <p class="fourth-block__services-cards__first-card__title">Техобслуживание</p>
+            <p class="fourth-block__services-cards__first-card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nascetur ultrices pellentesque vulputate sit. Ut feugiat.</p>
+            <button class="fourth-block__services-cards__first-card__description-button btn-out"><p class="btn-out__text">Подробнее</p></button>
+        </div>
+        <div class="fourth-block__services-cards__fourth-card">
+        <img class="fourth-block__services-cards__first-card__image" src="/image/car-check.svg" alt="carCheck">
+            <p class="fourth-block__services-cards__first-card__title">Диагностика и ремонт системы освещения</p>
+            <p class="fourth-block__services-cards__first-card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nascetur ultrices pellentesque vulputate sit. Ut feugiat.</p>
+            <button class="fourth-block__services-cards__first-card__description-button btn-out"><p class="btn-out__text">Подробнее</p></button>
+        </div>`
+
+    if (!(servicesCards.innerHTML == innerText)) {
+        servicesRightBtn.classList.add('btn-active')
+        servicesLeftBtn.classList.remove('btn-active')
+        servicesCards.innerHTML = innerText
+    }
+})
