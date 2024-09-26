caf65c6ba6cb5315fa14af8efe31afb9a5ce606f
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-26 22:20:25 +0500

    deleted index function and create __invoke function

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function index() {
+
+    public function __invoke() {
         return view('index');
     }
 }
