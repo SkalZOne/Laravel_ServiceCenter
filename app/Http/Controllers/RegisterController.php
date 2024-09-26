8471a8914ba95e0af01b9e0e768286410e5dfabb
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-27 01:05:20 +0500

    create users and messages array

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -10,6 +10,18 @@ use Validator;
 class RegisterController extends Controller
 {
     public function __invoke() {
+        $rules = [
+            'fio' => 'no_special_symbols',
+            'login' => 'min:3|unique:users',
+            'phone' => 'phone:RU',
+            'password' => 'upper_lower_case',
+        ];
 
+        $messages = [
+            'phone.phone' => 'Требуется правильный формат телефона',
+            'fio.no_special_symbols' => 'Нельзя использовать специальные символы',
+            'login.min' => 'Логин должен состоять минимум из 3-ех букв',
+            'password.upper_lower_case' => 'Обязательное присутствие хотя бы одной буквы в верхнем и нижнем регистре'
+        ];
 }
 }
