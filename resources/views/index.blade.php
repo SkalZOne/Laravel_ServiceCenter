495e31321bf908d41d20f5eb4d93e7d7f1446c5a
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-27 02:12:07 +0500

    add error messages in register page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -290,23 +290,36 @@
             <img src="{{ URL::asset('/image/red-dot.svg') }}" alt="dot">
             <img src="{{ URL::asset('/image/red-dot.svg') }}" alt="dot">
         </div>
+
         <div class="sixth-block__form__first-two-inputs">
             <div class="sixth-block__form__input-container">
                 <input name="fio" class="active-input" type="text" placeholder="ФИО">
-                <small><img src="{{ URL::asset('/image/warning.svg') }}" alt="warning">Имя пользователя имеет ошибку</small>
+                @if ($errors->has('fio'))
+                    <small><img src="{{ URL::asset('/image/warning.svg') }}" alt="warning">{{ $errors->first('fio') }}</small>
+                @endif
             </div>
             <div class="sixth-block__form__input-container">
                 <input name="login" type="text" placeholder="ЛОГИН">
+                @if ($errors->has('login'))
+                    <small><img src="{{ URL::asset('/image/warning.svg') }}" alt="warning">{{ $errors->first('login') }}</small>
+                @endif
             </div>
         </div>
         <div class="sixth-block__form__second-two-inputs">
             <div class="sixth-block__form__input-container">
                 <input name="phone" class="active-input" type="text" placeholder="ТЕЛЕФОН">
+                @if ($errors->has('phone'))
+                    <small><img src="{{ URL::asset('/image/warning.svg') }}" alt="warning">{{ $errors->first('phone') }}</small>
+                @endif
             </div>
             <div class="sixth-block__form__input-container">
                 <input name="password" type="password" placeholder="ПАРОЛЬ">
+                @if ($errors->has('password'))
+                    <small><img src="{{ URL::asset('/image/warning.svg') }}" alt="warning">{{ $errors->first('password') }}</small>
+                @endif
             </div>
         </div>
+
         <div class="sixth-block__form__buttons">
             <button class="sixth-block__form__btn-submit btn-out" type="submit">
                 <p class="btn-out__text">Зарегистрироваться</p>
