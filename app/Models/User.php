8a2b9166cd5137a6dcbc39077ffe533ee75cffcc
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-27 01:02:06 +0500

    create User model

---
+++ app/Models/User.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Hash;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class User extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [];
+    protected $guarded = [];
+}
