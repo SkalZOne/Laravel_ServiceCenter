b2f0a4640fc89dfc56ba13e7ca3530613320bf0b
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-27 01:34:58 +0500

    add route name for home page

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\IndexController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', IndexController::class);
+Route::get('/', IndexController::class)->name('home');
 
 Route::post('/register', RegisterController::class)->name('register');
