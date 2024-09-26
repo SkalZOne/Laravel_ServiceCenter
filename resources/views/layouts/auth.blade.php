d72fc4b38a4b91f2743f92d8e619aee474fb7fe9
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-27 02:29:14 +0500

    add auth layout

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <main class="main">
+        @yield('content')
+    </main>
+    <footer class="footer">
+        <p class="footer__text">2024 © Est et viverra pellentesque pharetra lorem proin in. Vitae magna at tempus commodo.</p>
+        <nav class="footer__navigation">
+            <div class="footer__navigation__links">
+                <a href="#" class="footer__navigation__link">Главная</a>
+                <a href="#" class="footer__navigation__link">(placeholder)</a>
+                <a href="#" class="footer__navigation__link">(placeholder)</a>
+                <a href="#" class="footer__navigation__link">(placeholder)</a>
+            </div>
+        </nav>
+    </footer>
+</body>
+
+</html>
