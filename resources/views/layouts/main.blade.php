828a573e4a637e438503fb142fb4748d71ed6ff2
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-26 21:57:15 +0500

    add footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -54,7 +54,17 @@
     <main class="main">
         @yield('content')
     </main>
-    <footer class="footer"></footer>
+    <footer class="footer">
+        <p class="footer__text">2024 © Est et viverra pellentesque pharetra lorem proin in. Vitae magna at tempus commodo.</p>
+        <nav class="footer__navigation">
+            <div class="footer__navigation__links">
+                <a href="#" class="footer__navigation__link">Главная</a>
+                <a href="#" class="footer__navigation__link">(placeholder)</a>
+                <a href="#" class="footer__navigation__link">(placeholder)</a>
+                <a href="#" class="footer__navigation__link">(placeholder)</a>
+            </div>
+        </nav>
+    </footer>
 </body>
 
 </html>
