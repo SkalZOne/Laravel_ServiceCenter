0064470913318bc6061ed64df8b5e75337c80007
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-27 01:03:34 +0500

    create register route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', IndexController::class);
+
+Route::post('/register', RegisterController::class)->name('register');
