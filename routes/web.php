722331fb0730a5c904bafc53075586abefc1518a
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-27 02:32:12 +0500

    create prefix group to auth controllers

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', IndexController::class)->name('home');
 
-Route::post('/register', RegisterController::class)->name('register');
+Route::prefix('auth')->group(function() {
+    Route::post('/register', RegisterController::class)->name('auth.register');
+});
