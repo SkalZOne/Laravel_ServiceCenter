3023b199db18653c2021c390571929d332db1dfb
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-27 01:06:45 +0500

    added names in form inputs

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -291,12 +291,12 @@
             <img src="{{ URL::asset('/image/red-dot.svg') }}" alt="dot">
         </div>
         <div class="sixth-block__form__first-two-inputs">
-            <input class="active-input" type="text" placeholder="ФИО">
-            <input type="text" placeholder="ЛОГИН">
+            <input name="fio" class="active-input" type="text" placeholder="ФИО">
+            <input name="login" type="text" placeholder="ЛОГИН">
         </div>
         <div class="sixth-block__form__second-two-inputs">
-            <input class="active-input" type="text" placeholder="ТЕЛЕФОН">
-            <input type="password" placeholder="ПАРОЛЬ">
+            <input name="phone" class="active-input" type="text" placeholder="ТЕЛЕФОН">
+            <input name="password" type="password" placeholder="ПАРОЛЬ">
         </div>
         <div class="sixth-block__form__buttons">
             <button class="sixth-block__form__btn-submit btn-out" type="submit"><p class="btn-out__text">Зарегистрироваться</p></button>
