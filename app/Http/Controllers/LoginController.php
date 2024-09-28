2488ceb3b97d4750b0e2bdd7b33fc41f4993e68d
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-28 18:03:14 +0500

    add logic to LoginController

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -10,7 +13,25 @@ class LoginController extends Controller
         return view('login');
     }
 
-    public function login() {
+    public function login(Request $request) {
+        $credentionals = $request->validate([
+            'login' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('login', $credentionals['login'])->first();
+
+        if(!$user || !Hash::check($credentionals['password'], $user->password)) {
+            $errors = [
+                'login' => 'Логин или пароль не верный'
+            ];
+
+            return redirect()->route('login')->withErrors($errors);
+        }
+
+        Auth::login($user, $request->filled('remember'));
+
+        dd (auth()->id());
 
     }
 }
