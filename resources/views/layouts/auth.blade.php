b7c285906dcc02c08296475375f49a0b26e79bd1
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-28 12:31:40 +0500

    Changed auth layout styles remove js link in head for avoid errors; added parent div: auth-container; added styles for auth-container

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,15 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/scss/app.scss', 'resources/css/app.css'])
 </head>
 
 <body>
-    <main class="main">
-        @yield('content')
-    </main>
+    <div class="auth-container">
+        <main class="main">
+            @yield('content')
+        </main>
+    </div>
     <footer class="footer">
-        <p class="footer__text">2024 © Est et viverra pellentesque pharetra lorem proin in. Vitae magna at tempus commodo.</p>
+        <p class="footer__text">2024 © Est et viverra pellentesque pharetra lorem proin in. Vitae magna at tempus
+            commodo.</p>
         <nav class="footer__navigation">
             <div class="footer__navigation__links">
                 <a href="#" class="footer__navigation__link">Главная</a>
