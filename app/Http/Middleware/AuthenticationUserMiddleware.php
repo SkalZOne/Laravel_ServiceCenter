c3f645b6bdc5a856caf1fa96d254c9a7cae6cd57
SkalZ <dima.tysyahnyy@mail.ru>
2024-09-30 08:43:02 +0500

    create AuthenticationUserMiddleware

---
+++ app/Http/Middleware/AuthenticationUserMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthenticationUserMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        dd(auth()->id());
+
+        return $next($request);
+    }
+}
