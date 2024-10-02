cda0d68bac24dd3e85dbf3c43e38339067282117
SkalZ <dima.tysyahnyy@mail.ru>
2024-10-02 22:10:07 +0500

    add foreach logic to orders index page

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -15,70 +15,29 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th>1</th>
-                <th>BMW</th>
-                <th id="tableDescription" class="description">
-                    <div class="description__container">
-                        <span class="description__container__text text-hidden">Lorem ipsum, dolor sit amet consectetur
-                            adipisicing elit. Debitis, velit.</span>
-                        <img id="arrowDown" class="description__container__arrow-down" src="/image/arrow-down.svg"
-                            alt="arrowDown" style="display: none">
-                    </div>
-                </th>
-                <th>05.03.2005</th>
-                <th>16:35-17:35</th>
-                <th class="open">Открыта</th>
-            </tr>
-            <tr>
-                <th>2</th>
-                <th>Honda Integra Type R</th>
-                <th id="tableDescription" class="description">
-                    <div class="description__container">
-                        <span class="description__container__text text-hidden">Lorem ipsum dolor sit amet consectetur
-                            adipisicing elit. Sit sapiente earum sunt? Cupiditate, minima dolor distinctio id, mollitia
-                            ratione, sapiente maiores officia accusantium saepe explicabo. Deleniti ea quod quas minima,
-                            praesentium eius, aperiam sit quia aliquid maxime, eaque at? Magnam doloribus quisquam
-                            mollitia nisi officiis possimus quis, et ad explicabo?</span>
-                        <img id="arrowDown" class="description__container__arrow-down" src="/image/arrow-down.svg"
-                            alt="arrowDown" style="display: none">
-                    </div>
-                </th>
-                <th>05.03.2005</th>
-                <th>16:35-17:35</th>
-                <th class="closed">Закрыта</th>
-            </tr>
-            <tr>
-                <th>3</th>
-                <th>Nissan Almera</th>
-                <th id="tableDescription" class="description">
-                    <div class="description__container">
-                        <span class="description__container__text text-hidden">Lorem ipsum dolor, sit amet consectetur
-                            adipisicing elit. Doloribus debitis, exercitationem suscipit odit totam obcaecati natus
-                            aliquid nobis eum error illum laboriosam soluta, enim, autem quo iusto officia rerum?
-                            Laboriosam in atque sequi a voluptatum eligendi quia officia optio tempore. Fuga harum
-                            aperiam, odio tenetur omnis ducimus ea placeat est obcaecati exercitationem aut dignissimos
-                            eos minus sapiente optio beatae deserunt id molestias rem et cupiditate tempora cum
-                            distinctio? Magnam distinctio sunt quidem saepe, eius error explicabo molestiae magni beatae
-                            non iusto, repellat tempore vero provident eligendi repellendus autem! Repellendus incidunt
-                            id laborum minima quis velit odit beatae recusandae adipisci voluptatibus. Blanditiis
-                            recusandae ea ratione alias esse. Quidem, ducimus rerum doloremque consequuntur ea neque
-                            porro officia voluptates nisi eos quo sint iste optio! In molestias dolorem ipsum vel
-                            voluptatem eligendi omnis, repellendus esse distinctio, aliquam tenetur cumque architecto
-                            eum, libero ipsam ullam error delectus quidem nisi! Magni, nobis vero voluptatum sint
-                            quaerat velit et consequuntur eveniet amet similique, earum autem, omnis cum cupiditate hic
-                            error tempore nostrum sapiente qui laudantium dolorem minus eum voluptates repellat!
-                            Expedita debitis sunt explicabo, perferendis eius sed consequatur provident. Magnam,
-                            veritatis! Dicta fuga aliquam laudantium animi, molestias, aspernatur amet rem veritatis,
-                            consequatur dolore vitae nobis modi!</span>
-                        <img id="arrowDown" class="description__container__arrow-down" src="/image/arrow-down.svg"
-                            alt="arrowDown" style="display: none">
-                    </div>
-                </th>
-                <th>05.03.2015</th>
-                <th>18:35-20:35</th>
-                <th class="canceled">Отменена</th>
-            </tr>
+            @foreach ($orders as $order)
+                <tr>
+                    <th>{{ $loop->index }}</th>
+                    <th>{{ $order->car }}</th>
+                    <th id="tableDescription" class="description">
+                        <div class="description__container">
+                            <span class="description__container__text text-hidden">{{ $order->description }}</span>
+                            <img id="arrowDown" class="description__container__arrow-down" src="/image/arrow-down.svg"
+                                alt="arrowDown" style="display: none">
+                        </div>
+                    </th>
+                    <th>{{ $order->date }}</th>
+                    <th>{{ $order->time }}</th>
+
+                    @if ($order->status == 1)
+                        <th class="open">Открыта</th>
+                    @elseif ($order->status == 2)
+                        <th class="closed">Закрыта</th>
+                    @elseif ($order->status == 3)
+                        <th class="canceled">Отменена</th>
+                    @endif
+                </tr>
+            @endforeach
         </tbody>
     </table>
 
