6978e153af2eefeed58627663e243ed1b8b12620
SkalZ <dima.tysyahnyy@mail.ru>
2024-10-02 21:57:47 +0500

    fix time selection fixed bug, while time became more than 21 and less than 8 hours, the time selection disappeared

--- resources/js/constants.js
+++ resources/js/constants.js
@@ -1,9 +1,21 @@
 const getDate = new Date();
 
-export const currentDate = {
+export let currentDate = {
     Month: getDate.getMonth() + 1,
     Year: getDate.getFullYear(),
     Day: getDate.getDate(),
     Hours: getDate.getHours(),
     Minutes: getDate.getMinutes(),
 };
+
+if (currentDate.Hours >= 21) {
+    currentDate.Day = currentDate.Day += 1;
+    currentDate.Hours = 8;
+} else {
+    for (let i = 0; i < 8; i++) {
+        if (currentDate.Hours == i) {
+            currentDate.Day = currentDate.Day += 1;
+            currentDate.Hours = 8;
+        }
+    }
+}
