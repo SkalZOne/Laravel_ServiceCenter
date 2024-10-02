e63efcc9be3e3f560cd71584300ff0cab50d928c
SkalZ <dima.tysyahnyy@mail.ru>
2024-10-02 22:10:00 +0500

    fix bug while orders list empty, js files were broken

--- resources/js/components/_table-description-slice.js
+++ resources/js/components/_table-description-slice.js
@@ -1,21 +1,41 @@
-for (let description of tableDescription) {
+let thDescription = document.getElementById('tableDescription');
 
-    if (description.textContent.length >= 250) {
-        let text = description.children[0].children[0]
-        let button = description.children[0].children[1]
+if (thDescription !== null) {
+    if (thDescription.length > 1) {
+        for (let description of thDescription) {
+            if (description.textContent.length >= 250) {
+                let text = description.children[0].children[0];
+                let button = description.children[0].children[1];
 
-        button.style.display = 'inline-block'
+                button.style.display = "inline-block";
 
-        button.addEventListener("click", function() {
-            if (text.classList.contains('text-hidden')) {
-                text.classList.remove('text-hidden')
-                button.style.transform = 'rotate(180deg)'
-            } else {
-                text.classList.add('text-hidden')
-                button.style.transform = 'rotate(0deg)'
+                button.addEventListener("click", function () {
+                    if (text.classList.contains("text-hidden")) {
+                        text.classList.remove("text-hidden");
+                        button.style.transform = "rotate(180deg)";
+                    } else {
+                        text.classList.add("text-hidden");
+                        button.style.transform = "rotate(0deg)";
+                    }
+                });
             }
-        })
+        }
+    } else {
+        if (thDescription.textContent.length >= 250) {
+            let text = thDescription.children[0].children[0];
+            let button = thDescription.children[0].children[1];
+
+            button.style.display = "inline-block";
+
+            button.addEventListener("click", function () {
+                if (text.classList.contains("text-hidden")) {
+                    text.classList.remove("text-hidden");
+                    button.style.transform = "rotate(180deg)";
+                } else {
+                    text.classList.add("text-hidden");
+                    button.style.transform = "rotate(0deg)";
+                }
+            });
+        }
     }
 }
-
-
