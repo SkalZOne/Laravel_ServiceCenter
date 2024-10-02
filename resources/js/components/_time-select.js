def5724a6061b30478dd47df15488c3ff2eccbfe
SkalZ <dima.tysyahnyy@mail.ru>
2024-10-02 18:01:40 +0500

    add time select logic

---
+++ resources/js/components/_time-select.js
@@ -0,0 +1,43 @@
+import { currentDate } from "../constants";
+
+let i = currentDate.Hours;
+
+for (i; i < 21; i++) {
+    let nextHour = i + 1;
+    let formatingHour = i + ":" + "00" + "-" + nextHour + ":" + "00";
+
+    timeSelect.insertAdjacentHTML(
+        "beforeend",
+        `<option value="${formatingHour}">${formatingHour}</option>`
+    );
+}
+
+[ daySelect, monthSelect, yearSelect ].forEach(function(element) {
+    element.addEventListener("change", function () {
+        if (
+            !(
+                daySelect.value == currentDate.Day &&
+                monthSelect.value == currentDate.Month &&
+                yearSelect.value == currentDate.Year
+            )
+        ) {
+            i = 8;
+        } else {
+            i = currentDate.Hours;
+        }
+    
+        while (timeSelect.children.length > 0) {
+            timeSelect.children[0].remove();
+        }
+    
+        for (i; i < 21; i++) {
+            let nextHour = i + 1;
+            let formatingHour = i + ":" + "00" + "-" + nextHour + ":" + "00";
+    
+            timeSelect.insertAdjacentHTML(
+                "beforeend",
+                `<option value="${formatingHour}">${formatingHour}</option>`
+            );
+        }
+    });
+})
--- resources/js/orders.js
+++ resources/js/orders.js
@@ -1,3 +1,4 @@
 import "./components/_date-selects"
 import "./components/_textarea-counter"
 import "./components/_table-description-slice"
+import "./components/_time-select"
