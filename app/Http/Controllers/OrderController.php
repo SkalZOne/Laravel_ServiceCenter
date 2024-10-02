b1f848657c70a3c39f427f03f52f90c6f8577e81
SkalZ <dima.tysyahnyy@mail.ru>
2024-10-02 21:50:13 +0500

    add create logic to OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,15 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Orders;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class OrderController extends Controller
 {
-    public function index() {
-        return view('orders');
+    public function index()
+    {
+        $userId = auth()->id();
+        $orders = Orders::where('user_id', $userId)->get()->all();
+
+        return view('orders', compact('orders'));
     }
 
-    public function create() {
-        dd(123123);
+    public function create(Request $request)
+    {
+        $validate = [
+            'car' => $request->car,
+            'date' => $request->year . '-' . $request->day . '-' . $request->month,
+            'description' => $request->description,
+            'time' => $request->time,
+            'user_id' => auth()->id()
+        ];
+
+        Orders::create($validate);
+
+        return redirect()->route('orders');
     }
 }
