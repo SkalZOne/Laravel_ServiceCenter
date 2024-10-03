0a02321be260f6ab52087412b599c5013b765b49
SkalZ <dima.tysyahnyy@mail.ru>
2024-10-03 13:12:08 +0500

    add old value to textarea-counter

--- resources/js/components/_textarea-counter.js
+++ resources/js/components/_textarea-counter.js
@@ -1,3 +1,9 @@
+import { url } from "../constants";
+
+if (url[4] != null) {
+    textareaCounter.textContent = textarea.value.length;
+}
+
 textarea.addEventListener("input", function () {
     textareaCounter.textContent = textarea.value.length;
 });
